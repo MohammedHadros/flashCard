fbd17dcf0e5326277fca0dc53f8df31e6601ee86
MohammedHadros <Hadros2017@gail.com>
2023-04-01 22:18:13 +0300

    Flash Card

---
+++ cards/urls.py
@@ -0,0 +1,13 @@
+# cards/urls.py
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path("",views.CardListView.as_view(),name="card-list"),
+    path("new",views.CardCreateView.as_view(),name="card-create"),
+    path("edit/<int:pk>",views.CardUpdateView.as_view(),name="card-update"),
+    path("delete/<int:pk>",views.CardDeleteView.as_view(),name="card-delete"),
+    path("box/<int:box_num>",views.BoxView.as_view(),name="box"),
+]
